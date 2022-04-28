0ad9528c044e29cad4f3837380c0780dcd755e59
jhcax <jhcax126@outlook.com>
2022-04-28 14:34:43 -0500

    skelecton

---
+++ environment.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Reversi:
+    def __init__(self) -> None:
+        self.board = [[0 for _ in range(8)] for _ in range(8)]
+        self.black_pieces = [(3, 3), (4, 4)]
+        self.white_pieces = [(3, 4), (4, 3)]
+        self.refresh_board()
+        self.black_move = True
+    
+    def refresh_board(self):
+        for p in self.black_pieces:
+            self.board[p[0]][p[1]] = 1
+        for p in self.white_pieces:
+            self.board[p[0]][p[1]] = -1
+        return self.board
+
+    def print_board(self):
+        print(' '.join([' ']+[str(i) for i in range(8)]))
+        for i in range(8):
+            print(str(i), end=' ')
+            for j in range(8):
+                if self.board[i][j] == 1:
+                    print('●', end=' ')
+                elif self.board[i][j] == -1:
+                    print('○', end=' ')
+                else:
+                    print(' ', end=' ')
+            print()
+
+    def is_valid_move(self):
+        pass
+
+    def find_valid_moves(self):
+        pass
+
+    def make_move(self,i,j):
+        self.board[i][j] = 1 if self.black_move else -1
+        if self.black_move:
+            self.black_pieces.append((i, j))
+        else:
+            self.white_pieces.append((i, j))
+        self.black_move = not self.black_move
+        self.print_board()
+
+    def endgame(self):
+        pass
