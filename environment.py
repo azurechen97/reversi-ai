a3a597c56651b527614fff738a13903e3ddea1e9
jhcax <jhcax126@outlook.com>
2022-04-29 13:35:30 -0500

    numpy

--- environment.py
+++ environment.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 class Reversi:
-    def __init__(self) -> None:
-        self.board = [[0 for _ in range(8)] for _ in range(8)]
-        self.black_pieces = {(3, 3), (4, 4)}
-        self.white_pieces = {(3, 4), (4, 3)}
+    def __init__(self, black_pieces={(3, 3), (4, 4)}, white_pieces={(3, 4), (4, 3)}) -> None:
+        self.board = np.zeros((8, 8), dtype=np.int8)
+        self.black_pieces = black_pieces
+        self.white_pieces = white_pieces
         self.refresh_board()
         self.current_player = 1
     
     def refresh_board(self):
         for p in self.black_pieces:
-            self.board[p[0]][p[1]] = 1
+            self.board[p] = 1
         for p in self.white_pieces:
-            self.board[p[0]][p[1]] = -1
+            self.board[p] = -1
         return self.board
 
     def print_board(self):
@@ -20,9 +20,9 @@ class Reversi:
         for i in range(8):
             print(str(i), end=' ')
             for j in range(8):
-                if self.board[i][j] == 1:
+                if self.board[i,j] == 1:
                     print('●', end=' ')
-                elif self.board[i][j] == -1:
+                elif self.board[i,j] == -1:
                     print('○', end=' ')
                 else:
                     print(' ', end=' ')
@@ -34,17 +34,17 @@ class Reversi:
             print("Out of bound!")
             return False
         # overlapping
-        if self.board[i][j]!=0:
+        if self.board[i,j]!=0:
             print("Overlapping!")
             return False
         # cannot flip anything
         # 180
         l = []
         for k in range(1,j+1):
-            if self.board[i][j-k] == -self.current_player:
+            if self.board[i,j-k] == -self.current_player:
                 l.append((i, j-k))
             else:
-                if self.board[i][j-k] == 0:
+                if self.board[i,j-k] == 0:
                     l = []
                 break
         can_flip.extend(l)
@@ -52,10 +52,10 @@ class Reversi:
         # 135
         l = []
         for k in range(1, min(i+1,j+1)):
-            if self.board[i-k][j-k] == -self.current_player:
+            if self.board[i-k,j-k] == -self.current_player:
                 l.append((i-k, j-k))
             else:
-                if self.board[i-k][j-k] == 0:
+                if self.board[i-k,j-k] == 0:
                     l = []
                 break
         can_flip.extend(l)
@@ -63,10 +63,10 @@ class Reversi:
         # 90
         l = []
         for k in range(1, i+1):
-            if self.board[i-k][j] == -self.current_player:
+            if self.board[i-k,j] == -self.current_player:
                 l.append((i-k, j))
             else:
-                if self.board[i-k][j] == 0:
+                if self.board[i-k,j] == 0:
                     l = []
                 break
         can_flip.extend(l)
@@ -74,10 +74,10 @@ class Reversi:
         # 45
         l = []
         for k in range(1, min(i+1, 8-j)):
-            if self.board[i-k][j+k] == -self.current_player:
+            if self.board[i-k,j+k] == -self.current_player:
                 l.append((i-k, j+k))
             else:
-                if self.board[i-k][j+k] == 0:
+                if self.board[i-k,j+k] == 0:
                     l = []
                 break
         can_flip.extend(l)
@@ -85,10 +85,10 @@ class Reversi:
         # 0
         l = []
         for k in range(1, 8-j):
-            if self.board[i][j+k] == -self.current_player:
+            if self.board[i,j+k] == -self.current_player:
                 l.append((i, j+k))
             else:
-                if self.board[i][j+k] == 0:
+                if self.board[i,j+k] == 0:
                     l = []
                 break
         can_flip.extend(l)
@@ -96,10 +96,10 @@ class Reversi:
         # -45
         l = []
         for k in range(1, min(8-i, 8-j)):
-            if self.board[i+k][j+k] == -self.current_player:
+            if self.board[i+k,j+k] == -self.current_player:
                 l.append((i+k, j+k))
             else:
-                if self.board[i+k][j+k] == 0:
+                if self.board[i+k,j+k] == 0:
                     l = []
                 break
         can_flip.extend(l)
@@ -107,10 +107,10 @@ class Reversi:
         # -90
         l = []
         for k in range(1, 8-i):
-            if self.board[i+k][j] == -self.current_player:
+            if self.board[i+k,j] == -self.current_player:
                 l.append((i+k, j))
             else:
-                if self.board[i+k][j] == 0:
+                if self.board[i+k,j] == 0:
                     l = []
                 break
         can_flip.extend(l)
@@ -118,10 +118,10 @@ class Reversi:
         # -135
         l = []
         for k in range(1, min(8-i, j+1)):
-            if self.board[i+k][j-k] == -self.current_player:
+            if self.board[i+k,j-k] == -self.current_player:
                 l.append((i+k, j-k))
             else:
-                if self.board[i+k][j-k] == 0:
+                if self.board[i+k,j-k] == 0:
                     l = []
                 break
         can_flip.extend(l)
@@ -140,14 +140,14 @@ class Reversi:
             print("Not a valid move!")
             return
 
-        self.board[i][j] = self.current_player
+        self.board[i,j] = self.current_player
         if self.current_player==1:
             self.black_pieces.add((i, j))
         else:
             self.white_pieces.add((i, j))
 
         for p in can_flip:
-            self.board[p[0]][p[1]] = self.current_player
+            self.board[p] = self.current_player
             if self.current_player == 1:
                 self.black_pieces.add(p)
                 self.white_pieces.remove(p)
